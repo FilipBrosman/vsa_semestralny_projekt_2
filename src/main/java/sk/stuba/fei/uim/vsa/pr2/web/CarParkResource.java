7406837eea9b24cc27f450f5bca2d71c053f64f9
Filip Brosman <xbrosman@stuba.sk>
2022-05-02 12:59:24 +0200

    Change: getAllCarParks, catch for ObjectNotFoundExcepsion.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/web/CarParkResource.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/web/CarParkResource.java
@@ -6,35 +6,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import sk.stuba.fei.uim.vsa.pr2.CarParkService;
+import sk.stuba.fei.uim.vsa.pr2.entity.CarPark;
+import sk.stuba.fei.uim.vsa.pr2.web.response.ObjectNotFoundException;
 
 import java.util.List;
 
-@Path("/carpark")
+@Path("/")
 public class CarParkResource {
     public static final String EMPTY_RESPONSE = "{}";
     private final ObjectMapper json = new ObjectMapper();
-
     private final CarParkService cps = new CarParkService();
 
     @GET
+    @Path("/carparks")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAllCarParks() {
-        List<Object> carparks = cps.getCarParks();
-
-        if (carparks.isEmpty())
-            return EMPTY_RESPONSE;
-
+    public Response getAllCarParks() {
         try{
-            return json.writeValueAsString(carparks);
+            List<Object> carparks = cps.getCarParks();
+            if (carparks == null) throw new ObjectNotFoundException();
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(json.writeValueAsString(carparks))
+                    .build();
         }
-        catch (JsonProcessingException e){
-            try{
-                return json.writeValueAsString(e.getMessage());
-            }
-            catch (JsonProcessingException ex){
-                // ignore
-                return EMPTY_RESPONSE;
-            }
+        catch (JsonProcessingException | ObjectNotFoundException e){
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 }
