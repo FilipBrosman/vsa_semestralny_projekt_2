32a6371c99e8c8a31d9ff07936e3b3b2a2d49179
Filip Brosman <xbrosman@stuba.sk>
2022-05-03 18:15:13 +0200

    Add: getter, setter to user.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/entity/User.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/entity/User.java
@@ -37,6 +37,34 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Long getId() {
         return id;
     }
