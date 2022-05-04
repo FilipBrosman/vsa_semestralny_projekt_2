bc0fa2b3605fb7563082bec094cd0702a2ad843f
Filip Brosman <xbrosman@stuba.sk>
2022-05-04 13:21:06 +0200

    Add: endReservation.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ReservationResource.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ReservationResource.java
@@ -11,6 +11,7 @@ import sk.stuba.fei.uim.vsa.pr2.web.request.ParkingSpotRequest;
 import sk.stuba.fei.uim.vsa.pr2.web.response.ObjectNotFoundException;
 
 import java.util.List;
+import java.util.Objects;
 
 @Path("/")
 public class ReservationResource extends AbstractResource {
@@ -26,6 +27,26 @@ public class ReservationResource extends AbstractResource {
 
             if (res == null) throw new ObjectNotFoundException();
 
+            return Response
+                    .status(Response.Status.CREATED)
+                    .entity(json.writeValueAsString(res))
+                    .build();
+        }
+        catch (JsonProcessingException | ObjectNotFoundException e){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @POST
+    @Path("/reservations/{id}/end")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response endReservation(@PathParam("id") Long id, String body) {
+        try{
+            if (!Objects.equals(body, "")) throw new ObjectNotFoundException();
+
+            Reservation res = (Reservation) cps.endReservation(id);
+            if (res == null) throw new ObjectNotFoundException();
             return Response
                     .status(Response.Status.CREATED)
                     .entity(json.writeValueAsString(res))
