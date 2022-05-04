2c050587012d49c9d77989779b912ea5d255f438
Filip Brosman <xbrosman@stuba.sk>
2022-05-04 14:52:27 +0200

    Add: resources to Project2Application.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/Project2Application.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/Project2Application.java
@@ -2,10 +2,8 @@ package sk.stuba.fei.uim.vsa.pr2;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import sk.stuba.fei.uim.vsa.pr2.web.CarParkFloorResource;
-import sk.stuba.fei.uim.vsa.pr2.web.CarParkResource;
+import sk.stuba.fei.uim.vsa.pr2.web.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +14,11 @@ public class Project2Application extends Application {
     static{
         appClasses.add(CarParkResource.class);
         appClasses.add(CarParkFloorResource.class);
+        appClasses.add(ParkingSpotsResource.class);
+        appClasses.add(CarResource.class);
+        appClasses.add(UserResource.class);
+        appClasses.add(ReservationResource.class);
+        appClasses.add(CouponResource.class);
     }
 
     @Override
