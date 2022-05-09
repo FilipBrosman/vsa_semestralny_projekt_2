55f77d2e39471f9bb2b5bbe2da642e96dab35a1e
Filip Brosman <xbrosman@stuba.sk>
2022-05-09 11:02:09 +0200

    Change: ParkingSpotRequest structure.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/web/request/ParkingSpotRequest.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/web/request/ParkingSpotRequest.java
@@ -1,13 +1,49 @@
 package sk.stuba.fei.uim.vsa.pr2.web.request;
 
 public class ParkingSpotRequest {
-    private String spotLabel;
+    private Long id;
+    private String identifier;
+    private String CarParkFloor;
+    private Long CarPark;
+    private Boolean free;
 
-    public String getSpotLabel() {
-        return spotLabel;
+    public Long getId() {
+        return id;
     }
 
-    public void setSpotLabel(String spotLabel) {
-        this.spotLabel = spotLabel;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getCarParkFloor() {
+        return CarParkFloor;
+    }
+
+    public void setCarParkFloor(String carParkFloor) {
+        CarParkFloor = carParkFloor;
+    }
+
+    public Long getCarPark() {
+        return CarPark;
+    }
+
+    public void setCarPark(Long carPark) {
+        CarPark = carPark;
+    }
+
+    public Boolean getFree() {
+        return free;
+    }
+
+    public void setFree(Boolean free) {
+        this.free = free;
     }
 }
