7b71f18d0f85861ee00c8e1628eb1b8029907070
Filip Brosman <xbrosman@stuba.sk>
2022-05-09 11:06:45 +0200

    Change: SpotLabel to Identifier in createParkingSpot.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ParkingSpotsResource.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ParkingSpotsResource.java
@@ -1,15 +1,12 @@
 package sk.stuba.fei.uim.vsa.pr2.web;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import sk.stuba.fei.uim.vsa.pr2.CarParkService;
 import sk.stuba.fei.uim.vsa.pr2.entity.CarPark;
 import sk.stuba.fei.uim.vsa.pr2.entity.ParkingSpot;
 import sk.stuba.fei.uim.vsa.pr2.web.request.ParkingSpotRequest;
-import sk.stuba.fei.uim.vsa.pr2.web.response.ObjectNotFoundException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -100,7 +97,7 @@ public class ParkingSpotsResource extends AbstractResource {
         try{
             ParkingSpotRequest psr = json.readValue(body, ParkingSpotRequest.class);
 
-            ParkingSpot ps = (ParkingSpot) cps.createParkingSpot(id, identifier, psr.getSpotLabel());
+            ParkingSpot ps = (ParkingSpot) cps.createParkingSpot(id, identifier, psr.getIdentifier());
             if (ps == null) return Response
                     .status(Response.Status.NOT_FOUND)
                     .build();
