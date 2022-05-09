77c380f10e7e2d44e45d680b30673104510bd296
Filip Brosman <xbrosman@stuba.sk>
2022-05-09 09:58:59 +0200

    Change: Check user in getMyReservarions.

--- src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ReservationResource.java
+++ src/main/java/sk/stuba/fei/uim/vsa/pr2/web/ReservationResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import sk.stuba.fei.uim.vsa.pr2.entity.ParkingSpot;
 import sk.stuba.fei.uim.vsa.pr2.entity.Reservation;
+import sk.stuba.fei.uim.vsa.pr2.entity.User;
 import sk.stuba.fei.uim.vsa.pr2.web.request.CreateReservationRequest;
 import sk.stuba.fei.uim.vsa.pr2.web.request.GetReservationsRequest;
 import sk.stuba.fei.uim.vsa.pr2.web.request.ParkingSpotRequest;
@@ -24,6 +25,8 @@ public class ReservationResource extends AbstractResource {
     public Response getMyReservations( @QueryParam("user") Long user ){
         try{
             if (user == null) throw new ObjectNotFoundException();
+            User u = (User) cps.getUser(user);
+            if (u == null) throw new ObjectNotFoundException();
 
             List<Object> res = cps.getMyReservations(user);
             if (res.isEmpty()) return Response
@@ -41,6 +44,8 @@ public class ReservationResource extends AbstractResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
+
+    //TODO: GET /reservations/{id}
 
     @POST
     @Path("/reservations")
